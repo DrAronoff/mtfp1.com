2563a0137b35837e9c0673691bb7c0326552b785
Jack Stange <jack@contfire.com>
2025-04-10 09:06:46 -0400

    updated contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,6 +11,13 @@ export function Contact() {
 <br></br>
 <br></br>
 <br></br>
+<a href="tel:7322510900,3" className="font-bold">(732) 251-0900 Ext. 3</a>
+<br></br>
+<a href="fireprevention@monroefd1nj.com" className="font-bold">fireprevention@monroefd1nj.com</a>
+<br></br>
+<br></br>
+
+
 
 <ul>
 <li><a href="http://www.monroefd1nj.com" target="new" rel="noopener noreferrer">Monroe Township Fire District #1 Board of Fire Commissioners</a></li>
